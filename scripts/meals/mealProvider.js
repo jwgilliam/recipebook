1d6ca95abc22421a79b5eae20e6416d314b59935
jwgilliam <ggsmoustache@gmail.com>
2023-05-28 16:30:27 -0500

    func: recipes can be created

---
+++ scripts/meals/mealProvider.js
@@ -0,0 +1,27 @@
+// create dropdown from list of meals in data
+
+let meals = []
+
+const setMeals = (mealsArray) => {
+  meals = mealsArray
+}
+
+export const useMeals = () => {
+  console.log("test")
+  return meals.slice()
+}
+
+export const getMeals = () => {
+  return fetch("http://localhost:8088/meals", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  }).then(response => response.json())
+    .then((mealsArray) => {
+      meals = mealsArray.slice()
+      console.log(meals)
+    })
+
+}
